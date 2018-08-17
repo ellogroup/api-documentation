243f451c1ea1d34d508df106592f00d246d3b74f
Dan Eggleston <daniel.eggleston@infinityworks.com>
2018-08-17 12:28:05 +0100

    NAV-1770: update readme

--- swagger/README.md
+++ swagger/README.md
@@ -4,7 +4,16 @@ Complete API documentation is authoritatively managed with [Swagger Hub](https:/
 
 ## Updating
 
-When a new version is published on Swagger Hub it should be updated here using:
+When a new version is published on Swagger Hub it can be updated here by running
 
 ```make update```
 
+in this directory.
+
+This will sync the latest published version of the API specs and update the following files:
+```
+membership-service-version.txt
+membership-service.yaml
+restaurant-service-version.txt
+restaurant-service.yaml
+```
--- swagger/scripts/get-latest.sh
+++ swagger/scripts/get-latest.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")/.."
 
-# get latest public version of the restaurant API and save to file
+# get latest public version of the an API and save to a .txt file
 get_version()
 {
     API_NAME=$1
@@ -14,10 +14,12 @@ get_version()
     > ${API_NAME}-version.txt
 }
 
+# Restaurant Service
 get_version restaurant-service
 RS_VERSION=`cat restaurant-service-version.txt`
 wget -O restaurant-service.yaml "https://app.swaggerhub.com/apiproxy/schema/file/the-dcg/restaurant-service/${RS_VERSION}/swagger.yaml"
 
+# Membership Service
 get_version membership-service
 MS_VERSION=`cat membership-service-version.txt`
 wget -O membership-service.yaml "https://app.swaggerhub.com/apiproxy/schema/file/the-dcg/membership-service/${MS_VERSION}/swagger.yaml"
